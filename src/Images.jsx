22f762cfd64f0341e7acef58f23d6c01df764871
이승택 <seungdeng17@gmail.com>
2022-01-12 17:30:00 +0900

    무한 스크롤 커스텀 훅

--- src/Images.jsx
+++ src/Images.jsx
@@ -1,52 +1,28 @@
-import { useRef, useState, useEffect, useCallback } from "react";
+import useInfiniteScroll from "./hooks/useInfiniteScroll";
 
 const LIMIT = 5;
 
-export default function Images() {
-  const [imageList, setImageList] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const observerRef = useRef(null);
+const getImages = async (startIndex, setItem, setIsFetching) => {
+  setIsFetching(true);
 
-  const getImages = async (startIndex) => {
-    setIsFetching(true);
+  const urlArr = [];
+  for (let i = startIndex; i < startIndex + LIMIT; i++) {
+    urlArr.push(`https://jsonplaceholder.typicode.com/photos/${i}`);
+  }
 
-    const urlArr = [];
-    for (let i = startIndex; i < startIndex + LIMIT; i++) {
-      urlArr.push(`https://jsonplaceholder.typicode.com/photos/${i}`);
-    }
-
-    await Promise.all(urlArr.map((url) => fetch(url)))
-      .then((resArr) => {
-        return Promise.all(resArr.map((res) => res.json()));
-      })
-      .then((jsonArr) => {
-        setImageList((v) => [...v, ...jsonArr]);
-      });
-
-    setIsFetching(false);
-  };
-
-  const onIntersect = useCallback(
-    ([entry]) => {
-      if (entry.isIntersecting && !isFetching) {
-        getImages(imageList.length + 1);
-      }
-    },
-    [isFetching, imageList]
-  );
-
-  useEffect(() => {
-    if (!observerRef.current) return;
-
-    const io = new IntersectionObserver(onIntersect, {
-      threshold: 0.4,
+  await Promise.all(urlArr.map((url) => fetch(url)))
+    .then((resArr) => {
+      return Promise.all(resArr.map((res) => res.json()));
+    })
+    .then((jsonArr) => {
+      setItem((v) => [...v, ...jsonArr]);
     });
-    io.observe(observerRef.current);
 
-    return () => {
-      io.disconnect();
-    };
-  }, [onIntersect]);
+  setIsFetching(false);
+};
+
+export default function Images() {
+  const [observerRef, imageList, isFetching] = useInfiniteScroll(getImages);
 
   return (
     <>
--- src/hooks/useInfiniteScroll.js
+++ src/hooks/useInfiniteScroll.js
@@ -1,1 +1,34 @@
-export default function useInfiniteScroll() {}
+import { useRef, useState, useEffect, useCallback } from "react";
+
+export default function useInfiniteScroll(
+  callback,
+  option = {
+    threshold: 0.4,
+  }
+) {
+  const [item, setItem] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
+  const ref = useRef(null);
+
+  const onIntersect = useCallback(
+    ([entry]) => {
+      if (entry.isIntersecting && !isFetching) {
+        callback(item.length + 1, setItem, setIsFetching);
+      }
+    },
+    [item, isFetching, callback]
+  );
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const io = new IntersectionObserver(onIntersect, option);
+    io.observe(ref.current);
+
+    return () => {
+      io.disconnect();
+    };
+  }, [onIntersect, option]);
+
+  return [ref, item, isFetching];
+}
